d68adaf78e9528c0202fd787cc68b086d9a3ac21
Maxime GELLE <maxime.gelle@gmail.com>
2020-05-11 19:34:13 +0200

    Add UsStateCode

---
+++ src/OrderTaking/Common/UsStateCode.hs
@@ -0,0 +1,20 @@
+module OrderTaking.Common.UsStateCode
+    ( create
+    , value
+    )
+where
+
+import           OrderTaking.Common.Result
+import           OrderTaking.Common.ConstrainedType
+
+-- A zip code
+data UsStateCode = UsStateCodeContent String deriving (Eq, Show)
+
+-- Return the string value inside a UsStateCode
+value :: UsStateCode -> String
+value (UsStateCodeContent str) = str
+
+-- Create a UsStateCode from a string
+-- Return Error if input is null, empty, or doesn't have 2 letters
+create :: String -> String -> Result UsStateCode
+create fieldName = createLike fieldName UsStateCodeContent "^(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY])$"
---
+++ test/OrderTaking/Common/UsStateCodeSpec.hs
@@ -0,0 +1,18 @@
+module OrderTaking.Common.UsStateCodeSpec ( spec ) where
+
+
+import Test.Hspec (Spec, hspec, describe, it, shouldBe)
+import           OrderTaking.Common.UsStateCode
+import           OrderTaking.Common.Result
+
+spec :: Spec
+spec = describe "UsStateCode" $
+    describe "Create a UsStateCode" $ do
+        it "should accept to create"
+            $ isOk (create "myfieldName" "FL") `shouldBe` True
+        it "should create a US state code with value"
+            $ valueR (create "myfieldName" "FL") `shouldBe` "FL"
+        it "should refuse bad code"
+            $ create "myfieldName" "AAA" `shouldBe` Error "myfieldName : 'AAA' must match the pattern '^(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY])$'"
+
+valueR (Ok a) = value a
