f8d33a4475009d8fa895b1c9735a100923f5641b
Maxime GELLE <maxime.gelle@gmail.com>
2020-05-11 19:51:54 +0200

    Add WidgetCode

---
+++ src/OrderTaking/Common/WidgetCode.hs
@@ -0,0 +1,20 @@
+module OrderTaking.Common.WidgetCode
+    ( create
+    , value
+    )
+where
+
+import           OrderTaking.Common.Result
+import           OrderTaking.Common.ConstrainedType
+
+-- A zip code
+data WidgetCode = WidgetCodeContent String deriving (Eq, Show)
+
+-- Return the string value inside a WidgetCode
+value :: WidgetCode -> String
+value (WidgetCodeContent str) = str
+
+-- Create an WidgetCode from a string
+-- Return Error if input is null. empty, or not matching pattern
+create :: String -> String -> Result WidgetCode
+create fieldName = createLike fieldName WidgetCodeContent "^W[0-9]{4}$"
