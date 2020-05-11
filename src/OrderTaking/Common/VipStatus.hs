ddd35dd43115bb6c3dd27e9bcc923c12de0be8c2
Maxime GELLE <maxime.gelle@gmail.com>
2020-05-11 19:05:10 +0200

    Add VipStatus

---
+++ src/OrderTaking/Common/VipStatus.hs
@@ -0,0 +1,25 @@
+module OrderTaking.Common.VipStatus
+    ( VipStatus(Normal, Vip)
+    , create
+    , value
+    )
+where
+
+import           OrderTaking.Common.Result
+
+-- Customer's VIP status
+data VipStatus = Normal | Vip  deriving (Eq, Show)
+
+-- Return a string representation of VipStatus 
+value :: VipStatus -> String
+value Normal = "Normal"
+value Vip    = "VIP"
+
+-- Create a VipStatus from a string
+-- Return Error if input is null, empty, or doesn't match one of the cases
+create :: String -> String -> Result VipStatus
+create _         "normal" = Ok Normal
+create _         "Normal" = Ok Normal
+create _         "vip"    = Ok Vip
+create _         "VIP"    = Ok Vip
+create fieldName _ = Error $ fieldName ++ ": Must be one of 'Normal', 'VIP'"
---
+++ test/OrderTaking/Common/VipStatusSpec.hs
@@ -0,0 +1,24 @@
+module OrderTaking.Common.VipStatusSpec ( spec ) where
+
+
+import Test.Hspec (Spec, hspec, describe, it, shouldBe)
+import           OrderTaking.Common.VipStatus
+import           OrderTaking.Common.Result
+
+spec :: Spec
+spec = describe "VipStatus" $ do
+    describe "Create a vip status" $ do
+        it "should accept to create" $ do
+            isOk (create "myfieldName" "normal") `shouldBe` True
+        it "should create a VipStatus normal" $ do
+            valueR (create "myfieldName" "normal") `shouldBe` Normal
+        it "should create a VipStatus Normal" $ do
+            valueR (create "myfieldName" "Normal") `shouldBe` Normal
+        it "should create a VipStatus vip" $ do
+            valueR (create "myfieldName" "vip") `shouldBe` Vip
+        it "should create a VipStatus VIP" $ do
+            valueR (create "myfieldName" "VIP") `shouldBe` Vip
+        it "should return an error otherwise" $ do 
+            create "myfieldName" "text" `shouldBe` Error "myfieldName: Must be one of 'Normal', 'VIP'"
+
+valueR (Ok a) = a
